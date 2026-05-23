385db11e9a65af87b1d73e6e4ed101a79ee80cf8
Jiseop Lee <ljseop1030@gmail.com>
2026-05-24 00:03:06 +0900

    fix: omitted nickname requirements and useless id extraction

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,13 +7,13 @@ const jwt = require("jsonwebtoken");
 // [POST] /api/auth/register
 async function register(req, res) {
     try {
-        const { username, password } = req.body;
-        if( !username || !password ){
-            return res.status(400).json({error: "username and password required"});
+        const { username, nickname, password } = req.body;
+        if (!username || !nickname || !password) {
+            return res.status(400).json({ error: "username, nickname and password required" });
         }
+        // password hashing
         const password_hash = await bcrypt.hash(password, 10);
-        const result = await model.create(username, nickname, password_hash);
-        const id = result.id
+        const result = await model.create({username, nickname, password_hash});
         res.status(201).json({ id: result.lastID, username, nickname });
     } catch (err) {
         if (err instanceof model.UsernameTakenError) {
@@ -28,23 +28,23 @@ async function register(req, res) {
 // POST /api/auth/login
 async function login(req, res) {
     try {
-        const { username, nickname, password } = req.body;
-        if (!username || !password) return res.status(400).json({ error: "username and password required" })
+        const { username, password } = req.body;
+        if (!username || !password) return res.status(400).json({ error: "username and password required" });
 
         const user = await model.findByUsername(username); // find user
-        if (!user) return res.status(401).json({error: "Invalid usename"});
+        if (!user) return res.status(401).json({error: "Invalid username"});
 
         const ok = await bcrypt.compare(password, user.password_hash); // Check password
         if(!ok) return res.status(401).json({error: "Wrong password"});
 
         // if success; provide tokens
-        const token = jwt.sign( // sign a JWT containing the user’s id and username
-             {id: user.id, username: user.username },
-             process.env.JWT_SECRET,
-             { expiresIn: process.env.JWT_EXPIRES_IN }
+        const token = jwt.sign( // sign a JWT containing the user's id and username
+            {id: user.id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
         );
 
-        res.status(200).json({token, username: user.username});
+        res.status(200).json({ token, user: { id: user.id, username: user.username, nickname: user.nickname } });
     } catch (err) {
         console.error(err);
         res.status(500).json({error:"Server error"});
