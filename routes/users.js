26b45c6e8b20d69bf944941c32bfcc8f205e235f
Jiseop Lee <ljseop1030@gmail.com>
2026-05-25 01:16:05 +0900

    fix: add authentication for protected GET /me

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/userController");
 
+const { authenticate } = require("../middleware/authMiddleware"); // for authentication
+
 // add /users(prefix) in app.js
-router.get("/me", controller.getMe);
+router.get("/me", authenticate, controller.getMe);
 
 module.exports = router;
